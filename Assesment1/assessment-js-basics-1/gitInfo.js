9e737b87b47d69a747da83035f2f24494bb333fa
Jared Neal <jared.neal@me.com>
2023-02-10 10:57:21 -0700

    Add files via upload

---
+++ Assesment1/assessment-js-basics-1/gitInfo.js
@@ -0,0 +1,83 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+var gitDefinition =
+  "open source control system designed to handle everything from small to large projects with speed";
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+var gitHubDefinition =
+  "is a version-control platform for users to collaborate or adopt open source code projects, fork code, and share ideas ";
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+var gitInitDefinition =
+  "this command creates a new git repository.  It can be used to convert an existing, unversioned project to a git respositor or initialize a new empty one.";
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+var gitCloneDefinition =
+  "points to an exisiting repo and makes a clone or copy of that repo in a new directory at another location";
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+var gitStatusDefinition =
+  "displays the state of the working directory and the staging area.  It lets you see which change have been staged, which haven't, and which files aren't being tracked";
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+var gitAddDefinition =
+  "adds a change in the working directory to the staging area.  It tells git that you want to include updates in the next commit.";
+var gitAddCode = "git add --all";
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+var gitCommitDefinition =
+  "captures a snapshot of the project's currently staged changes";
+var gitCommitCode = "";
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+var gitPushDefinition = "upload local repo content to a remote repo";
---
+++ Assesment1/assessment-js-basics-1/sldc.js
@@ -0,0 +1,36 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning =
+  "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added.";
+
+const analysis_of_requirments =
+  "this stage means getting input from client and programmers.  Learn the strengths and weaknesses of the current system with improvement as the goal";
+
+const design =
+  "starts by turning the software specifications into a design plan. It is important to have a plan for collecting and incorporating input.";
+
+const implementation =
+  "at this stage, actual develpment start.  It's important to stick to the agreed blueprint and make sure proper guidelines are in place about the code style and practices";
+
+const testing_and_integration =
+  "test for defect and deficiencies.  Fix issues until the product meets specifications.";
+
+const maintain =
+  "finding bugs, fixing bugs, addressing bug reports, monitor performance ";
